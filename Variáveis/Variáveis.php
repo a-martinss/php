061d4e1d63a1696fb754c34a601bbe3c4a6418b4
Adeilson Martins <adeilsonmartinslima@hotmail.com>
2024-07-14 07:36:02 -0300

    Variáveis

---
+++ Variáveis/Variáveis.php
@@ -0,0 +1,25 @@
+<?php 
+  //Variáveis
+  $nome = "Adeilson"; //String
+  $_nome = "Adeilson"; //String
+  $Nome = "Adeilson"; //String
+  $valor = 123456; //Inteiro
+  $nota = 9.5; //Float
+  $idade = 29; // Inteiro
+  $preco = 12.50; // Ponto flutuante
+  $ativo = true; // Booleano
+  $cores = array("Cinza", "Roxo", "Vermelho"); // Array
+  $pessoa = array("Adeilson", "29", "M", 1.70); // Array
+
+  //Exibindo as Variáveis
+  echo "nome = $nome <br>";
+  echo "nome = $_nome <br>";
+  echo "nome = $Nome <br>";
+  echo "valor = $valor <br>";
+  echo "nota = $nota <br>";
+  echo "idade = $idade <br>";
+  echo "preco = $preco <br>";
+  echo "ativo = $ativo <br>";
+  echo "cores = $cores[0] $cores[1] $cores[2] <br>";
+  echo "pessoa = $pessoa[0], $pessoa[1], $pessoa[2] <br>";
+?> 
